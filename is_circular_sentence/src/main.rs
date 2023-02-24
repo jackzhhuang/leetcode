680e10ff320b80091c52ceb815ec32b1cb356844
jackzhhuang <jackzhuang@gmail.com>
2023-02-24 14:29:57 +0800

    finish is_circular_sentence

--- is_circular_sentence/src/main.rs
+++ is_circular_sentence/src/main.rs
@@ -1,3 +1,28 @@
+
+struct Solution {
+
+}
+
+impl Solution {
+    pub fn is_circular_sentence(sentence: String) -> bool {
+        let v = sentence.chars().collect::<Vec<char>>();
+        for w in v.windows(3).into_iter() {
+            if w[1] != ' ' {
+                continue;
+            }
+            if w[0] != w[2] {
+                return false;
+            }
+        }
+        return v[0] == v[v.len() - 1];
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let result = Solution::is_circular_sentence("leetcode exercises sound delightful".to_string());
+    if result {
+        println!("is_circular_sentence returned true");
+    } else {
+        println!("is_circular_sentence returned false");
+    }
 }
