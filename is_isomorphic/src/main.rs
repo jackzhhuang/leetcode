e4cd7813a59511455501a89c7ee21852835049cb
jackzhhuang <jackzhuang@gmail.com>
2023-02-25 15:51:30 +0800

    finish is_isomorphic by using vec as a hash table

--- is_isomorphic/src/main.rs
+++ is_isomorphic/src/main.rs
@@ -2,32 +2,18 @@ struct Solution {
 
 }
 impl Solution {
-    fn inner(s: &str, t: &String) -> bool {
-        let mut hash = std::collections::HashMap::new();
-
-        for (left, right) in s.chars().zip(t.chars()) {
-            let r_op = hash.get(&left);
-            match r_op {
-                None => {
-                    hash.insert(left, right);
-                }
-                Some(r) => {
-                    if *r != right {
-                        return false;
-                    }
-                }
-            }
-        }
-
-        true
-    }
     pub fn is_isomorphic(s: String, t: String) -> bool {
-        if !Solution::inner(&s, &t) {
-            return false;
-        }
-        if !Solution::inner(&t, &s) {
-            return false;
-        }
+        let mut left_hash = vec![s.len(); 256];
+        let mut right_hash = vec![t.len(); 256];
+
+        for (i, (left, right)) in s.bytes().zip(t.bytes()).enumerate() {
+            if left_hash[left as usize] != right_hash[right as usize] {
+                return false;
+            }
+            left_hash[left as usize] = i;
+            right_hash[right as usize] = i;
+       }
+
         true
     }
 }
