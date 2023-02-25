7df4e0b837b82642d69921f263121c64c473572c
jackzhhuang <jackzhuang@gmail.com>
2023-02-25 15:12:08 +0800

    finish is_isomorphic using zip

--- is_isomorphic/src/main.rs
+++ is_isomorphic/src/main.rs
@@ -5,17 +5,14 @@ impl Solution {
     fn inner(s: &str, t: &String) -> bool {
         let mut hash = std::collections::HashMap::new();
 
-        let lefts = s.chars().collect::<Vec<char>>();
-        let rights = t.chars().collect::<Vec<char>>();
-
-        for index in 0..lefts.len() {
-            let r_op = hash.get(&lefts[index]);
+        for (left, right) in s.chars().zip(t.chars()) {
+            let r_op = hash.get(&left);
             match r_op {
                 None => {
-                    hash.insert(lefts[index], rights[index]);
+                    hash.insert(left, right);
                 }
                 Some(r) => {
-                    if *r != rights[index] {
+                    if *r != right {
                         return false;
                     }
                 }
@@ -35,7 +32,7 @@ impl Solution {
     }
 }
 
-fn main() {
+fn test() {
     let result = Solution::is_isomorphic("egg".to_string(), "add".to_string());
     println!("is_isomorphic returned: {}", result);
 
@@ -47,4 +44,8 @@ fn main() {
 
     let result = Solution::is_isomorphic("badc".to_string(), "baba".to_string());
     println!("is_isomorphic returned: {}", result);
+}
+
+fn main() {
+    test();
 }
