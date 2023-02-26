2e91d12cc5829a9bcb16e78e64ee8a92910fb299
jackzhhuang <jackzhuang@gmail.com>
2023-02-26 21:23:48 +0800

    finish find_ocurrences

--- find_ocurrences/src/main.rs
+++ find_ocurrences/src/main.rs
@@ -1,3 +1,21 @@
+struct Solution {
+
+}
+
+impl Solution {
+    pub fn find_ocurrences(text: String, first: String, second: String) -> Vec<String> {
+        let vector_string: Vec<&str> = text.split_whitespace().collect();
+        vector_string.windows(3).filter_map(|x| {
+            if x[0].eq(&first) && x[1].eq(&second) {
+                Some(x[2].to_string())
+            } else {
+                None
+            }
+        }).collect()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("the answer is {:?}", Solution::find_ocurrences("alice is a good girl she is a good student".to_string(), "a".to_string(), "good".to_string()));
+    println!("the answer is {:?}", Solution::find_ocurrences("we will we will rock you".to_string(), "we".to_string(), "will".to_string()));
 }
