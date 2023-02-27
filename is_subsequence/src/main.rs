919abde35bbdae91a68446c677256c6d3d4ca11a
jackzhhuang <jackzhhuang@gmail.com>
2023-02-27 13:09:17 +0800

    finish is_subsequence

--- is_subsequence/src/main.rs
+++ is_subsequence/src/main.rs
@@ -1,3 +1,22 @@
+struct Solution {
+
+}
+
+impl Solution {
+    pub fn is_subsequence(s: String, t: String) -> bool {
+        let mut iter = t.chars();
+        s.chars().filter(|x| {
+            while let Some(y) = iter.next() {
+                if x == &y {
+                    return true;
+                }
+            }
+            false
+        }).collect::<String>().len() == s.len()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("abc is ahbgdc subsequence: {}", Solution::is_subsequence("abc".to_string(), "ahbgdc".to_string()));
+    println!("axc is ahbgdc subsequence: {}", Solution::is_subsequence("axc".to_string(), "ahbgdc".to_string()));
 }
