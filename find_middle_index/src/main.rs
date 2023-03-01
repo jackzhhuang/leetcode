aac9e490b13336a2ae96a69e918c82750bfc6f76
jackzhhuang <jackzhhuang@gmail.com>
2023-03-01 10:49:46 +0800

    finish find_middle_index

--- find_middle_index/src/main.rs
+++ find_middle_index/src/main.rs
@@ -1,3 +1,38 @@
+struct Solution {
+
+} 
+
+impl Solution {
+    pub fn find_middle_index(nums: Vec<i32>) -> i32 {
+        let mut right_sum = nums.iter().sum::<i32>() - nums[0];
+        let mut left_sum = 0;
+        let mut index = 0;
+        loop {
+            if right_sum == left_sum {
+                return index as i32;
+            }
+            index += 1;
+            if index >= nums.len() {
+                break;
+            }
+            left_sum += nums[index - 1];
+            right_sum -= nums[index];
+        }
+        return -1;
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let input = vec![2, 3, -1, 8, 4];
+    println!("mid index is {}", Solution::find_middle_index(input));
+
+    let input = vec![1, -1, 4];
+    println!("mid index is {}", Solution::find_middle_index(input));
+
+    let input = vec![2, 5];
+    println!("mid index is {}", Solution::find_middle_index(input));
+
+    let input = vec![1];
+    println!("mid index is {}", Solution::find_middle_index(input));
+
+
 }
