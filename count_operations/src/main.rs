74fd828960a40b311cc76c8d0422da8b03e8875e
jackzhhuang <jackzhhuang@gmail.com>
2023-03-07 14:50:44 +0800

    finish count_operations

--- count_operations/src/main.rs
+++ count_operations/src/main.rs
@@ -1,3 +1,28 @@
+struct Solution;
+impl Solution {
+    fn co(mut num1: i32, mut num2: i32) -> i32 {
+        let mut count = 0;
+        loop {
+            if num1 == 0 || num2 == 0 {
+                return count;
+            }
+            if num1 >= num2 {
+                num1 = num1 - num2;
+                count += 1;
+            } else {
+                num2 = num2 - num1;
+                count += 1;
+            }
+        }
+    }
+    pub fn count_operations(num1: i32, num2: i32) -> i32 {
+        Solution::co(num1, num2)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", Solution::count_operations(5, 4));
+    println!("{}", Solution::count_operations(2, 3));
+    println!("{}", Solution::count_operations(10, 10));
+    println!("{}", Solution::count_operations(0, 0));
 }
